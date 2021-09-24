dc9c0c1123f78ebf20e8465befc098bdd7e87465
Linh B Ngo <lngo@wcupa.edu>
2021-09-23 20:07:08 -0400

    Update setup_spark_master.sh

--- setup_spark_master.sh
+++ setup_spark_master.sh
@@ -8,20 +8,22 @@ echo "Number of workers: ${workers}"
 sudo apt-get update
 sudo apt-get install -y nfs-kernel-server
 
-wget --no-verbose https://ftp.wayne.edu/apache/spark/spark-3.0.3/spark-3.0.3-bin-hadoop3.2.tgz
-tar xzf spark-3.0.3-bin-hadoop3.2.tgz
-mv spark-3.0.3-bin-hadoop3.2 /opt/
-rm spark-3.0.3-bin-hadoop3.2.tgz
 
-sudo chown nobody:nogroup /opt/spark-3.0.3-bin-hadoop3.2
-sudo chmod -R a+rwx /opt/spark-3.0.3-bin-hadoop3.2
+
+sudo mkdir -p /opt/software
+sudo chown nobody:nogroup /opt/software
+sudo chmod -R a+rwx /opt/software
 
 for i in 2 .. ${workerS}
 do
-  echo "/opt/spark-3.0.3-bin-hadoop3.2 192.168.1.${i}(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+  echo "/opt/software 192.168.1.${i}(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 done
 
 sudo systemctl restart nfs-kernel-server
 
+wget --no-verbose https://ftp.wayne.edu/apache/spark/spark-3.0.3/spark-3.0.3-bin-hadoop3.2.tgz
+tar xzf spark-3.0.3-bin-hadoop3.2.tgz
+mv spark-3.0.3-bin-hadoop3.2 /opt/software/
+rm spark-3.0.3-bin-hadoop3.2.tgz
 
-sudo touch /opt/spark_master_done
+sudo touch /opt/software/DONE
