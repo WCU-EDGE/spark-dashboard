a88d2e6eb587d4bb3c4e5297db1e8adb4c3820dd
Linh B Ngo <lngo@wcupa.edu>
2021-10-19 21:03:00 -0400

    Update setup_spark_workers.sh

--- setup_spark_workers.sh
+++ setup_spark_workers.sh
@@ -11,3 +11,8 @@ while [ ! -f /opt/software/DONE ]; do
   sudo mount 192.168.1.1:/opt/software /opt/software
   sleep 10
 done
+
+
+#export JAVA_HOME=/opt/software/jdk-11.0.12+7
+#lngo@head:/opt/software/jdk-11.0.12+7$ cd /opt/software/spark-3.0.3-bin-hadoop3.2/
+#./bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port 8080 spark://192.168.1.1:7077 >> /opt/software/spark-worker.out
