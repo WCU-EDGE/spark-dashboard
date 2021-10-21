97a213feb0927080be56b181ca15656265380015
Linh B Ngo <lngo@wcupa.edu>
2021-10-21 19:47:22 -0400

    Update setup_user.sh

--- setup_user.sh
+++ setup_user.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+apt-get update
+apt-get install -qqq -y python3 python3-pip python3-numpy python3-matplotlib python3-scipy python3-pandas python3-simpy ipython3
+pip3 install tweepy feedparser
+update-alternatives --install "/usr/bin/python" "python" "$(which python3)" 1
+
+echo "Password: " $1
+mypass=$(python3 -c "import crypt; print(crypt.crypt('$1', crypt.mksalt(crypt.METHOD_SHA512)))")
+useradd -m -p $mypass -s /bin/bash rammy
