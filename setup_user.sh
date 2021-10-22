d427258dbab8ff13ed883c6c0626490bfe8f58a3
Linh B Ngo <lngo@wcupa.edu>
2021-10-21 20:45:40 -0400

    Update setup_user.sh

--- setup_user.sh
+++ setup_user.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get install -qqq -y python3 python3-pip python3-numpy python3-matplotlib python3-scipy python3-pandas python3-simpy ipython3
-pip3 install tweepy feedparser
+pip3 install tweepy feedparser jupyter Pygments
 update-alternatives --install "/usr/bin/python" "python" "$(which python3)" 1
 
 echo "Password: " $1
