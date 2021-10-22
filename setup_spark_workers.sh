a9f6101fad639717a205c8ecce6a40828bf21195
Linh B Ngo <lngo@wcupa.edu>
2021-10-21 22:06:07 -0400

    Update setup_spark_workers.sh

--- setup_spark_workers.sh
+++ setup_spark_workers.sh
@@ -16,7 +16,7 @@ echo 'export JAVA_HOME=/opt/software/jdk-11.0.12+7' >> /home/rammy/.bashrc
 echo 'export SPARK_HOME="/opt/software/spark-3.0.3-bin-hadoop3.2/"' >> /home/rammy/.bashrc
 echo 'export PATH=$JAVA_HOME/bin:$SPARK_HOME/bin:$PATH' >> /home/rammy/.bashrc
 chown rammy: /home/rammy/.bashrc
-sudo -H -u rammy bash -c '/opt/software/spark-3.0.3-bin-hadoop3.2/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port 8080 spark://192.168.1.1:7077 >> /opt/software/spark-worker.out &' &
+sudo -H -u rammy bash -c 'source $HOME/.bashrc && spark-class org.apache.spark.deploy.worker.Worker --webui-port 8080 spark://192.168.1.1:7077 >> /opt/software/spark-worker.out &' &
 
 #export JAVA_HOME=/opt/software/jdk-11.0.12+7
 #lngo@head:/opt/software/jdk-11.0.12+7$ cd /opt/software/spark-3.0.3-bin-hadoop3.2/
