999d7f77509dbcb0bc8284b700a08ee5b11ec465
Linh B Ngo <lngo@wcupa.edu>
2021-11-01 13:34:02 -0400

    Update setup_grafana.sh

--- setup_grafana.sh
+++ setup_grafana.sh
@@ -12,7 +12,7 @@ sudo apt-get install grafana
 
 # Changing port from default to port 8080
 sudo sed -i 's/;http_port = 3000/http_port = 8080/' /etc/grafana/grafana.ini
-sudo sed -i 's/;admin_password = admin/admin_password = $1/' /etc/grafana/grafana.ini
+sudo sed -i "s/;admin_password = admin/admin_password = $1/" /etc/grafana/grafana.ini
 
 # Starting grafana server and enabling start at boot
 sudo systemctl daemon-reload
