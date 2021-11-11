db74a79f54db7c2c7c554ebd1c497b95bca5d258
Linh B Ngo <lngo@wcupa.edu>
2021-11-11 10:24:29 -0500

    Update setup_broker.sh

--- setup_broker.sh
+++ setup_broker.sh
@@ -2,13 +2,6 @@ set -x
 sudo apt-get update
 
 sudo mkdir -p /opt/software
-# sudo apt-get install -y nfs-kernel-server
-# #WIP - set JAVA_HOME and ensure every user who ssh's in knows where it is
-# while IFS= read -r line; do
-#   echo 'export JAVA_HOME=users/$line/jdk-11.0.12+7' >> /users/$line/.bashrc
-#   chown $line: /users/$line/.bashrc
-# done < <( ls -l /users | grep 4096 | cut -d' ' -f3 )
-
 
 wget --no-verbose https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.12%2B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.12_7.tar.gz
 tar -xzf OpenJDK11U-jdk_x64_linux_hotspot_11.0.12_7.tar.gz
@@ -25,6 +18,5 @@ echo 'export JAVA_HOME=/opt/software/jdk-11.0.12+7'
 # chown rammy: /home/rammy/.bashrc
 
 cd /opt/software/kafka_2.13-3.0.0
-bin/zookeeper-server-start.sh config/zookeeper.properties
-bg
-bin/kafka-server-start.sh config/server.properties
+bin/zookeeper-server-start.sh -daemon /local/repository/kafka/zookeeper.properties
+bin/kafka-server-start.sh -daemon /local/repository/kafka/server.properties
